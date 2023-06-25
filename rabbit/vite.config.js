5c0086de72dcb7e855652f0a2f266bd05f96a691
whm <1305816215@qq.com>
2023-06-25 22:38:17 +0800

    config color

--- rabbit/vite.config.js
+++ rabbit/vite.config.js
@@ -3,17 +3,31 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({ /* options */ }),
-    Components({ /* options */ }),
+    Components({
+      resolvers: [
+        ElementPlusResolver({importStyle:"sass"}),
+      ]
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
