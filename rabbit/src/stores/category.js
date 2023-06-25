f7776d9b09cf94695859b9962ffdf6ce9e984f49
whm <1305816215@qq.com>
2023-06-26 01:29:10 +0800

    pinia优化请求

---
+++ rabbit/src/stores/category.js
@@ -0,0 +1,18 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import { getCategoryAPI } from '@/apis/layout'
+
+export const useCategoryStore = defineStore('category', () => {
+    //state
+    const categoryList = ref([])
+
+    //action
+    const getCategory = async () => {
+        const res = await getCategoryAPI()
+        categoryList.value = res.result
+    }
+    return {
+        categoryList,
+        getCategory
+    }
+})
