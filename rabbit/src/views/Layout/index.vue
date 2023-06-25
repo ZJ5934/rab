3fef0b3006f409a9f2533b67f2b91426628809c5
whm <1305816215@qq.com>
2023-06-26 00:41:23 +0800

    layout静态结构搭建

--- rabbit/src/views/Layout/index.vue
+++ rabbit/src/views/Layout/index.vue
@@ -1,6 +1,12 @@
-<template>
-    <div>
-        我是首页
-        <router-view></router-view>
-    </div>
-</template>
+<script setup>
+    import LayoutNav from './components/LayoutNav.vue'
+    import LayoutHeader from './components/LayoutHeader.vue'
+    import LayoutFooter from './components/LayoutFooter.vue'
+    </script>
+    
+    <template>
+      <LayoutNav />
+      <LayoutHeader />
+      <RouterView />
+      <LayoutFooter />
+    </template>
