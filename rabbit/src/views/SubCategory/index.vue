afd99fd408994d61ff235f695081fb81612db77b
whm <1305816215@qq.com>
2023-06-29 11:29:50 +0800

    列表筛选功能实现

--- rabbit/src/views/SubCategory/index.vue
+++ rabbit/src/views/SubCategory/index.vue
@@ -29,6 +29,11 @@
   onMounted(() => {
     getGoodList()
   })
+
+  const tabChange = () => {
+    reqData.value.page = 1
+    getGoodList()
+  }
 </script>
 
 <template>
@@ -43,7 +48,7 @@
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
