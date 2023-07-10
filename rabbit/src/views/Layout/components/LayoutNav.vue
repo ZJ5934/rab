3f9074c7265c4f6aa65151b17570f2a4c70af167
whm <1305816215@qq.com>
2023-07-10 13:01:23 +0800

    多模板适配登录状态

--- rabbit/src/views/Layout/components/LayoutNav.vue
+++ rabbit/src/views/Layout/components/LayoutNav.vue
@@ -1,13 +1,14 @@
 <script setup>
-
+  import { useUserStore } from '@/stores/user.js'
+  const userStore = useUserStore()
 </script>
 
 <template>
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>whm</a></li>
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{userStore.userInfo.account}}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
@@ -30,36 +31,38 @@
 
 
 <style scoped lang="scss">
-.app-topnav {
-  background: #333;
-  ul {
-    display: flex;
-    height: 53px;
-    justify-content: flex-end;
-    align-items: center;
-    li {
-      a {
-        padding: 0 15px;
-        color: #cdcdcd;
-        line-height: 1;
-        display: inline-block;
+  .app-topnav {
+    background: #333;
 
-        i {
-          font-size: 14px;
-          margin-right: 2px;
-        }
+    ul {
+      display: flex;
+      height: 53px;
+      justify-content: flex-end;
+      align-items: center;
 
-        &:hover {
-          color: $xtxColor;
-        }
-      }
-
-      ~li {
+      li {
         a {
-          border-left: 2px solid #666;
+          padding: 0 15px;
+          color: #cdcdcd;
+          line-height: 1;
+          display: inline-block;
+
+          i {
+            font-size: 14px;
+            margin-right: 2px;
+          }
+
+          &:hover {
+            color: $xtxColor;
+          }
+        }
+
+        ~li {
+          a {
+            border-left: 2px solid #666;
+          }
         }
       }
     }
   }
-}
 </style>
