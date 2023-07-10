4362cf0b20021ecb90da9085062ab6c1dbbd5663
whm <1305816215@qq.com>
2023-07-10 10:42:41 +0800

    自定义校验规则

--- rabbit/src/views/Login/index.vue
+++ rabbit/src/views/Login/index.vue
@@ -5,6 +5,7 @@
 const form = ref({
     account:'',
     password:'',
+    agree:true,
 })
 
 //2.准备规则对象
@@ -15,6 +16,17 @@ const rules = {
     password:[
         {required:true,message:'密码不能为空',trigger:'blur'},
         {min:6,max:14,message:'密码长度为6-14字符',trigger:'blur'},
+    ],
+    agree:[
+        {
+            validator:(rule,value,callback) => {
+                if(value){
+                    callback()
+                }else{
+                    callback(new Error('请勾选协议'))
+                }
+            }
+        }
     ]
 }
 </script>
@@ -49,8 +61,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  v-model="form.agree" size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
