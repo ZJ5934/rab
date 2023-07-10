173de83e2dae2230f5de9987de79b6a195d739d0
whm <1305816215@qq.com>
2023-07-10 13:33:40 +0800

    解决token 401失效拦截问题

--- rabbit/src/utils/http.js
+++ rabbit/src/utils/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user.js'
-
+import router from '@/router'
 
 
 //基础封装axios
@@ -25,11 +25,17 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+    const userStore = useUserStore()
     //统一错误提示
     ElMessage({
         type: 'warning',
         message:e.response.data.message
     })
+    //401token失效处理
+    if (e.response.status === 401) { 
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
     return Promise.reject(e)
 })
 
