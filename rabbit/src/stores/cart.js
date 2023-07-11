923aa7ef1b6bd4c98e591c57eb392652df468fa7
whm <1305816215@qq.com>
2023-07-11 23:04:21 +0800

    接口购物车-删除购物车

--- rabbit/src/stores/cart.js
+++ rabbit/src/stores/cart.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import { computed } from "vue";
 import { useUserStore } from './user.js'
-import { insertCartAPI, findNewCartListAPI } from '@/apis/cart.js'
+import { insertCartAPI, findNewCartListAPI, delCartAPI } from '@/apis/cart.js'
 
 export const useCartStore = defineStore('cart', () => {
     const userStore = useUserStore()
@@ -10,12 +10,16 @@ export const useCartStore = defineStore('cart', () => {
     const cartList = ref([])
 
     //action
+    const updateNewList = async () => { 
+        const res = await findNewCartListAPI()
+        cartList.value = res.result
+    }
+    
     const addCart = async (goods) => {
         const { skuId, count } = goods
         if (isLogin.value) {
             await insertCartAPI({ skuId, count })
-            const res = await findNewCartListAPI()
-            cartList.value = res.result
+            updateNewList()
         } else {
             const item = cartList.value.find((item) => item.skuId === goods.skuId)
             if (item) {
@@ -26,9 +30,15 @@ export const useCartStore = defineStore('cart', () => {
         }
     }
 
-    const delCart = (skuId) => {
-        const idx = cartList.value.findIndex((item) => item.skuId === skuId)
-        cartList.value.splice(idx, 1)
+    const delCart = async (skuId) => {
+        if (isLogin.value) {
+            await delCartAPI([skuId])
+            updateNewList()
+        } else {
+            const idx = cartList.value.findIndex((item) => item.skuId === skuId)
+            cartList.value.splice(idx, 1)
+        }
+
     }
 
     const singleCheck = (skuId, selected) => {
