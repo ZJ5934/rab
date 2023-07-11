7772d462d8e60e738121e18ec82b70f78a6034dc
whm <1305816215@qq.com>
2023-07-11 22:06:18 +0800

    本地购物车-统计计算

--- rabbit/src/stores/cart.js
+++ rabbit/src/stores/cart.js
@@ -10,29 +10,32 @@ export const useCartStore = defineStore('cart', () => {
         const item = cartList.value.find((item) => item.skuId === goods.skuId)
         if (item) {
             item.count += goods.count
-        } else { 
+        } else {
             cartList.value.push(goods)
         }
     }
 
-    const delCart = (skuId) => { 
+    const delCart = (skuId) => {
         const idx = cartList.value.findIndex((item) => item.skuId === skuId)
-        cartList.value.splice(idx,1)
+        cartList.value.splice(idx, 1)
     }
 
-    const singleCheck = (skuId,selected) => { 
+    const singleCheck = (skuId, selected) => {
         const item = cartList.value.find((item) => item.skuId === skuId)
         item.selected = selected
     }
 
-    const allCheck = (selected) => { 
-        cartList.value.forEach((item)=>item.selected = selected)
+    const allCheck = (selected) => {
+        cartList.value.forEach((item) => item.selected = selected)
     }
 
     const isAll = computed(() => cartList.value.every((item) => item.selected))
 
-    const allCount = computed(() => cartList.value.reduce((a,c) => a + c.count ,0))
-    const allPrice = computed(() => cartList.value.reduce((a,c) => a + c.count * c.price ,0))
+    const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+    const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
+    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count, 0))
+    const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count * c.price, 0))
 
     return {
         cartList,
@@ -42,8 +45,10 @@ export const useCartStore = defineStore('cart', () => {
         allPrice,
         singleCheck,
         isAll,
-        allCheck
+        allCheck,
+        selectedCount,
+        selectedPrice
     }
 }, {
-    persist:true
+    persist: true
 })
