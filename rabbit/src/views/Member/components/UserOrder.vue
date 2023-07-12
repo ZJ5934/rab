00e6b733cc5cc69833d287ae480d00a717005afd
whm <1305816215@qq.com>
2023-07-12 18:35:58 +0800

    会员中心-我的订单基础渲染

--- rabbit/src/views/Member/components/UserOrder.vue
+++ rabbit/src/views/Member/components/UserOrder.vue
@@ -1,4 +1,20 @@
 <script setup>
+    import { getUserOrder } from '@/apis/order.js'
+
+    const orderList = ref([])
+    const params = ref({
+        orderState:0,
+        page:1,
+        pageSize:2
+    })
+    const getOrderList = async () => {
+        const res = await getUserOrder(params.value)
+        orderList.value = res.result
+    }
+
+    onMounted(() => {
+        getOrderList()
+    })
     // tab列表
     const tabTypes = [
         { name: "all", label: "全部订单" },
@@ -10,7 +26,6 @@
         { name: "cancel", label: "已取消" }
     ]
     // 订单列表
-    const orderList = []
 
 </script>
 
@@ -26,7 +41,7 @@
                 </div>
                 <div v-else>
                     <!-- 订单列表 -->
-                    <div class="order-item" v-for="order in orderList" :key="order.id">
+                    <div class="order-item" v-for="order in orderList.items" :key="order.id">
                         <div class="head">
                             <span>下单时间：{{ order.createTime }}</span>
                             <span>订单编号：{{ order.id }}</span>
